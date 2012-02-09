86496a55bf7b78dabc5153a695bfb08c0fc22e5c
Barry Wark <barrywark@gmail.com>
2012-02-09 15:01:45 -0500

    Jenkins test harness 2

--- test/jenkins-test.sh
+++ test/jenkins-test.sh
@@ -3,4 +3,4 @@
 echo "Running tests with $MATLAB..."
 export PATH=$MATLAB_ROOT/bin:$PATH
 touch startup.m
-matlab -nodisplay -nodesktop -r "addpath(getenv('MATLAB_XUNIT_PATH'));javaaddpath(getenv('OVATION_JAR_PATH')); runtestsuite test; exit(0)"
+matlab -nodisplay -nodesktop -r "addpath('test');addpath(getenv('MATLAB_XUNIT_PATH'));javaaddpath(getenv('OVATION_JAR_PATH')); runtestsuite test; exit(0)"
