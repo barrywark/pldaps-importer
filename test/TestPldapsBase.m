13787bd3a7d80bd3126b3ff8f976200b5ce8dbe5
Barry Wark <barrywark@gmail.com>
2012-04-05 22:56:54 -0500

    Added swappable fixture to pldapsbase
    The tmp dots fixture appears to have the wrong number of strobe_times
    in the plx structure

--- test/TestPldapsBase.m
+++ test/TestPldapsBase.m
@@ -9,10 +9,16 @@ classdef TestPldapsBase < TestCase
     
     properties(Constant)
         
+%         pdsFile = 'fixtures/jlyTest040212tmpdots1109.PDS';
+%         plxFile = 'fixtures/jlyTest040212tmpDots1103.mat';
+%         plxRawFile = 'fixtures/jlyTest040212tmpDots1103.plx';
+%         plxExpFile = 'fixtures/jlyTest040212tmpdots1109.exp';
+        
         pdsFile = 'fixtures/jlyTest040212tmpSaccadeMapping1102.PDS';
         plxFile = 'fixtures/jlyTest040212tmpSaccadeMapping1103.mat';
         plxRawFile = 'fixtures/jlyTest040212tmpSaccadeMapping1103.plx';
         plxExpFile = 'fixtures/jlyTest040212tmpdots1109.exp';
+        
         timezone = org.joda.time.DateTimeZone.forID('US/Central');
     end
     
